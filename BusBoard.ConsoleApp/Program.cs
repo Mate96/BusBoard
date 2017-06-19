4682da3a5009b022cd753e43cd0c36f73cbf4e64
Mate96 <kovacs.mate1121@gmail.com>
2017-06-19 18:18:03 +0100

    BusBoard

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace BusBoard.ConsoleApp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
+
+
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://api.tfl.gov.uk");
+
+            var request = new RestRequest();
+            request.Resource = "StopPoint/490008660N/Arrivals";
+            
+            IRestResponse<List<Bus>> response = client.Execute<List<Bus>>(request);
+            var result = client.Execute<List<Bus>>(request).Data;
+
+            var resultNames = result.Select(b => b.expectedArrival + b.destinationName);
+
+            //for (i=0; i<resultNames.Count; i++)
+
+            Console.WriteLine(string.Concat(resultNames));
+            Console.ReadLine();
+        }
+
     }
-  }
+
+    public class Bus
+    {
+        public DateTime expectedArrival { get; set; }
+        public string destinationName { get; set; }
+    }
+    
 }
