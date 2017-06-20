143ef05f038865e76af970954e1c9c804d14d570
Mate96 <kovacs.mate1121@gmail.com>
2017-06-20 14:04:38 +0100

    BusBoard

---
+++ BusBoard.ConsoleApp/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusBoard.ConsoleApp
+{
+    public class Bus
+    {
+        public DateTime expectedArrival { get; set; }
+        public string lineName { get; set; }
+        public string destinationName { get; set; }
+        public string towards { get; set; }
+    }
+
+    public class Postcode
+    {
+        public string longitude { get; set; }
+        public string latitude { get; set; }
+    }
+
+    public class PcodeResult
+    {
+        public Postcode result { get; set; }
+    }
+
+    public class Stops
+    {
+        public string naptanId { get; set; }
+        public short distance { get; set; }
+    }
+
+    public class StopsOuter
+    {
+        public List<Stops> stopPoints { get; set; }
+    }
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -12,24 +12,14 @@ namespace BusBoard.ConsoleApp
     {
 
 
-        static void Main(string[] args)
+        static void Main()
         {
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var api = new api();
 
-            //request and extract coordinates of postcode
-            var client = new RestClient();
-            client.BaseUrl = new Uri("http://api.postcodes.io");
-            var requestPcode = new RestRequest();
-            Console.WriteLine("Enter postcode:");
-            requestPcode.Resource = "/postcodes/" + Console.ReadLine();
-            IRestResponse<PcodeResult> responsePcode = client.Execute<PcodeResult>(requestPcode);
-
-            //request nearest bus stop IDs to the received coordinates
-            client.BaseUrl = new Uri("https://api.tfl.gov.uk");
-            var requestStops = new RestRequest();
-            requestStops.Resource = "StopPoint?stopTypes=NaptanPublicBusCoachTram&lat=" + responsePcode.Data.result.latitude + "&lon=" + responsePcode.Data.result.longitude;
-            IRestResponse<StopsOuter> responseStops = client.Execute<StopsOuter>(requestStops);
-
+            Console.WriteLine("Enter postcode");
+            List<string> coordinates = api.Pcode(Console.ReadLine());
+            List<Stops> Stops = api.Stops(coordinates);
+            
 
             bool ask = true;
             int n = 0;
@@ -47,21 +37,18 @@ namespace BusBoard.ConsoleApp
                 }
             }
 
-            if(responseStops.Data.stopPoints.Count < n)
+            if(Stops.Count < n)
             {
-                n = responseStops.Data.stopPoints.Count;
+                n = Stops.Count;
                 Console.WriteLine("Only " + n + " stops found within 1000m");
             }
+
+
 
             //display the 5 next buses stopping at the number of stops requested/found
             for (var i = 0; i < n; i++)
             {
-                var requestBuses = new RestRequest();
-                requestBuses.Resource = "StopPoint/" + responseStops.Data.stopPoints[i].naptanId + "/Arrivals";
-                IRestResponse<List<Bus>> response = client.Execute<List<Bus>>(requestBuses);
-                var result = client.Execute<List<Bus>>(requestBuses).Data;
-
-                var resultNames = result
+                var resultNames = api.Bus(Stops, i)
                     .OrderBy(b => b.expectedArrival).ThenBy(b => b.expectedArrival).ToList()    //order buses in from soonest to arrive to latest to arrive
                     .Select(b => "Expected arrival: " + b.expectedArrival.Hour + ":" + b.expectedArrival.Minute + "\n" + "Line: " + b.lineName + "\n" + "Destination: " + b.destinationName + "\n" + "Towards: " + b.towards + "\n \n")      //transform to a list of strings
                     .Take(5);       //select first 5 buses
@@ -73,33 +60,46 @@ namespace BusBoard.ConsoleApp
 
     }
 
-    public class Bus
+
+    public class api        //functions responsible for calling apis
     {
-        public DateTime expectedArrival { get; set; }
-        public string lineName { get; set; }
-        public string destinationName { get; set; }
-        public string towards { get; set; }
-    }
-    
-    public class Postcode
-    {
-        public string longitude { get; set; }
-        public string latitude { get; set; }
+        RestClient client = new RestClient();
+        public api()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;           
+        }
+
+        public List<string> Pcode(string pcode)
+        {
+            //request and extract coordinates of postcode           
+            client.BaseUrl = new Uri("http://api.postcodes.io");
+            var request = new RestRequest();
+            request.Resource = "/postcodes/" + pcode;
+            IRestResponse<PcodeResult> response = client.Execute<PcodeResult>(request);
+            List<string> coordinates = new List<string>();
+            coordinates.Add(response.Data.result.latitude);
+            coordinates.Add(response.Data.result.longitude);
+            return coordinates;
+        }
+
+
+        public List<Stops> Stops(List<string> coordinates)
+        {
+            //request nearest bus stop IDs to the received coordinates
+            client.BaseUrl = new Uri("https://api.tfl.gov.uk");
+            var request = new RestRequest();
+            request.Resource = "StopPoint?stopTypes=NaptanPublicBusCoachTram&lat=" + coordinates[0] + "&lon=" + coordinates[1];
+            IRestResponse<StopsOuter> response = client.Execute<StopsOuter>(request);
+            return response.Data.stopPoints;
+        }
+
+        public List<Bus> Bus(List<Stops> Stops, int i)
+        {
+            var request = new RestRequest();
+            request.Resource = "StopPoint/" + Stops[i].naptanId + "/Arrivals";
+            IRestResponse<List<Bus>> response = client.Execute<List<Bus>>(request);
+            return client.Execute<List<Bus>>(request).Data;
+        }
     }
 
-    public class PcodeResult
-    {
-        public Postcode result { get; set; }
-    }
-
-    public class Stops
-    {
-        public string naptanId { get; set; }
-        public short distance { get; set; }
-    }
-
-    public class StopsOuter
-    {
-        public List<Stops> stopPoints { get; set; }
-    }
 }
