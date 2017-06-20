282e94bd934e9dfe1707f1581fe9dd696da9b968
Mate96 <kovacs.mate1121@gmail.com>
2017-06-20 10:36:56 +0100

    BusBoard

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -19,16 +19,21 @@ namespace BusBoard.ConsoleApp
             client.BaseUrl = new Uri("https://api.tfl.gov.uk");
 
             var request = new RestRequest();
-            request.Resource = "StopPoint/490008660N/Arrivals";
+            Console.WriteLine("Enter stop code:");
+            request.Resource = "StopPoint/" + Console.ReadLine() + "/Arrivals";
+
             
-            IRestResponse<List<Bus>> response = client.Execute<List<Bus>>(request);
+
+            IRestResponse <List<Bus>> response = client.Execute<List<Bus>>(request);
             var result = client.Execute<List<Bus>>(request).Data;
 
-            var resultNames = result.Select(b => b.expectedArrival + b.destinationName);
-
+            var resultNames = result
+                .OrderBy(b => b.expectedArrival).ThenBy(b => b.expectedArrival).ToList()
+                .Select(b => "Expected arrival: " + b.expectedArrival.Hour + ":" + b.expectedArrival.Minute + "\n" + "Line:" + b.lineName + "\n" + "Destination: " + b.destinationName + "\n" + "Towards: " + b.towards + "\n \n")
+                .Take(5);
             //for (i=0; i<resultNames.Count; i++)
 
-            Console.WriteLine(string.Concat(resultNames));
+            Console.WriteLine("\n" + string.Concat(resultNames));
             Console.ReadLine();
         }
 
@@ -37,7 +42,14 @@ namespace BusBoard.ConsoleApp
     public class Bus
     {
         public DateTime expectedArrival { get; set; }
+        public string lineName { get; set; }
         public string destinationName { get; set; }
+        public string towards { get; set; }
     }
     
+    public class postcode
+    {
+        public string longitude { get; set; }
+        public string latitude { get; set; }
+    }
 }
